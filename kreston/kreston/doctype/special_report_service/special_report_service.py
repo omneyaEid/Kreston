33a9ad6da8a555e60c8855136b28b2112b055923
mostafa12 <eng.mostafamahmoud1992@gmail.com>
2024-10-04 23:56:56 +0000

    Lead

---
+++ kreston/kreston/doctype/special_report_service/special_report_service.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, omneyaeid827@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SpecialReportService(Document):
+	pass
---
+++ kreston/kreston/doctype/zakat_service/zakat_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, omneyaeid827@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Zakat Service', {
+	// refresh: function(frm) {
+
+	// }
+});
